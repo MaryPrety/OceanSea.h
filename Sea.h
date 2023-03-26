61809a6ab75c9538bbf22f624da6090f3c3e94a4
MaryPrety <116995383+MaryPrety@users.noreply.github.com>
2023-03-26 10:34:10 +0300

    Update Sea.h

--- Sea.h
+++ Sea.h
@@ -1,18 +1,41 @@
-#pragma once
-#include "Ocean.h"
-class Sea :
-	public Ocean
+#include <iostream>
+#include <string>
+#include "Zaliv.h"
+
+using namespace std;
+
+class Sea: private Zaliv
 {
 private:
 	string name;
 	float deep;
 	float size;
+
+	int zalivs_count = 0;
+
+	string NamesSea[12] = {
+	"Китайское",
+	"Элегенское",
+	"Унарное",
+	"Арабское",
+	"Кабанье",
+	"Сильное",
+	"Франзенское",
+	"Белорусское",
+	"Батинское",
+	"Мирное",
+	"Седьмое",
+	"Парамайское"
+	};
+
 public:
+	Zaliv* zalivs = new Zaliv[zalivs_count];
+
 	Sea()
 	{
-		name = "??????????";
-		deep = 1;
-		size = 1;
+		name = NamesSea[rand() % 12];
+		deep = rand() % 8500 + 3500;
+		size = (rand() % 100000 + 20000) / 10.0;
 	}
 	Sea(string N, float D, float S)
 	{
@@ -21,10 +44,12 @@ public:
 		size = S;
 	}
 
+	void addZaliv();
+
 	void setName(string N);
 	void setDeep(float D);
 	void setSize(float S);
 
 	friend ostream& operator<<(ostream& stream, const Sea obj);
-	friend istream& operator<<(istream& stream, Sea obj);
-}; #pragma once
+	friend istream& operator>>(istream& stream, Sea& obj);
+};
